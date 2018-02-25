59e816f43cd49b8b4e47ba2eca2cc05721681952
Pavan Sabnis <pvnsbns@gmail.com>
2018-02-25 02:03:56 -0800

    Increments questionNumber and calls nextQuestion() for the next question when an answer is selected. Within the nextQuestion() method, I check to see if questionNumber is out of range. If this is the case, I spawn a UIAlwerController with a message and a restart button prompted to ask if the user wishes to start over by calling startOver().

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -7,11 +7,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
     //Place your instance variables here
+    
     let allQuestions = QuestionBank() //Don't forget parenthesis when init() has no parameters!
     //Merely represents the answer of the button that the user pressed
     var pickedAnswer: Bool = false
+    //represents the current question being asked. REPRESENTS THE STATE
+    var questionNumber : Int = 0
     
     
     @IBOutlet weak var questionLabel: UILabel!
@@ -21,13 +23,13 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let firstQuestion = allQuestions.list[0]
+        // present the first question
+        let firstQuestion = allQuestions.list[questionNumber] // This used to be zero
         questionLabel.text = firstQuestion.questionText
         
     }
 
-
+    // When either true or false button is pressed
     @IBAction func answerPressed(_ sender: AnyObject) {
         if sender.tag == 1 {
             pickedAnswer = true
@@ -36,6 +38,16 @@ class ViewController: UIViewController {
             pickedAnswer = false
         }
         checkAnswer()
+        questionNumber += 1
+//        if questionNumber < 12 {
+//            questionNumber += 1
+//        }
+//        else {
+//            questionNumber = 0
+//        }
+        nextQuestion()
+        // You can also use print statements in the console to debug
+        
     }
     
     
@@ -45,12 +57,32 @@ class ViewController: UIViewController {
     
 
     func nextQuestion() {
-        
+        // Update the UILabel to display the current question
+        if questionNumber < 13 {
+            questionLabel.text = allQuestions.list[questionNumber].questionText
+        }
+        else {
+//            print("end of quiz")
+//            // Land back at the first question if end is reached
+//            questionNumber = 0
+            let alert = UIAlertController(title: "Congrats!", message: "You reached the end of the quiz, would you like to start over?", preferredStyle: .alert)
+            // the handler represents what happens when the button is pressed
+            // when you see "in", make sure you use "self"
+            let restartAction = UIAlertAction(title: "Restart", style: .default, handler: { (UIAlertAction) in
+                self.startOver()
+            })
+            // add the newly-created action to the alert UIAlertController
+            alert.addAction(restartAction)
+            // present the alert to the user
+            // "completion" is what to do after UIAlertController is presented. nil for this case.
+            present(alert, animated: true, completion: nil)
+            
+        }
     }
     
     
     func checkAnswer() {
-        let correctAnswer = allQuestions.list[0].answer
+        let correctAnswer = allQuestions.list[questionNumber].answer
         
         if pickedAnswer == correctAnswer {
             print("You got the correct answer!")
